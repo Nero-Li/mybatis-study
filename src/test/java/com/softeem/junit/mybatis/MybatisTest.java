195cc760c5a6bf13c81c6ebc54cd1995a898f366
Nero_Li <375892799@qq.com>
2022-08-29 16:34:56 +0800

    mybatis调用存储过程

--- src/test/java/com/softeem/junit/mybatis/MybatisTest.java
+++ src/test/java/com/softeem/junit/mybatis/MybatisTest.java
@@ -1,6 +1,7 @@
 package com.softeem.junit.mybatis;
 
 
+import com.softeem.dao.TStudentMapper;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -10,6 +11,7 @@ import org.junit.Test;
 import java.io.IOException;
 import java.io.Reader;
 import java.sql.Connection;
+import java.util.HashMap;
 
 /**
  * description:
@@ -32,6 +34,34 @@ public class MybatisTest {
             sqlSession = sqlSessionFactory.openSession();
             Connection connection = sqlSession.getConnection();
             System.out.println(connection);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            if (sqlSession != null) {
+                //如果type="POOLED",代表使用连接池,close则是将连接收回到连接池中
+                //如果type="UNPOOLED",代表直连,close则会调用Connecttion.close方法关闭连接
+                sqlSession.close();
+            }
+        }
+    }
+
+
+    @Test
+    public void callPro(){
+        SqlSession sqlSession = null;
+        try {
+            //利用Reader加载classPath下的mybatis-config.xml配置文件
+            Reader reader = Resources.getResourceAsReader("mybatis-config.xml");
+            //利用构造者模式根据配置文件信息初始化SqlSessionFactory对象
+            SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+            //创建SqlSession对象
+            sqlSession = sqlSessionFactory.openSession();
+            HashMap<String, Object> param = new HashMap<>();
+            param.put("id",1);
+            TStudentMapper mapper = sqlSession.getMapper(TStudentMapper.class);
+            mapper.callSelectStu(param);
+            System.out.println("查询结果:"+param.get("stuName"));
+
         } catch (IOException e) {
             e.printStackTrace();
         }finally {
